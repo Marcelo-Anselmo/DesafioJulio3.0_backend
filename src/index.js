87d7b80ce664b0d883176be2339543363672d468
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-01-06 12:21:45 -0300

    fix-bcryptDepedences

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const { connectToMongo } = require("./database/connect");
