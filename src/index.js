98728fdd603923d8c3f315edcbf09ea7abacf611
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-01-08 13:19:16 -0300

    set-bcrypt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const { connectToMongo } = require("./database/connect");
